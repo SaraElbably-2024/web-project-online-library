34bd0eef10fa3e3b9f2f9dfff52e795655c749bc
Amira Ayman <mirayman0011@gmail.com>
2026-04-18 16:39:44 +0200

    add icon_title_change background color-add js file

---
+++ home.js
@@ -0,0 +1,11 @@
+  document.getElementById("signupBtn").onclick = function () {
+        window.location.href = "signup.html";
+    };
+
+    document.getElementById("loginBtn").onclick = function () {
+        window.location.href = "login.html";
+    };
+
+    document.getElementById("getStartedBtn").onclick = function () {
+        window.location.href = "signup.html";
+    };
